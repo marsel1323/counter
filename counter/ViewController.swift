9550e1364f50f1178a1e1859fa9546cde03f69e3
Marsel.Abdrakhmanov <marsel.abdrakhmanov@easycomm.ru>
2023-02-26 15:59:01 +0300

    Add label, counter button and reset button; Implement counter logic

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var counter = 0
+    
+    @IBOutlet weak var resetCounterButton: UIButton!
+    @IBOutlet weak var countButton: UIButton!
+    @IBOutlet weak var counterLabelView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabelView.text = "Значение счётчика: \(counter)"
     }
 
 
+    @IBAction func countButtonDidTup(_ sender: Any) {
+        counter += 1
+        counterLabelView.text = "Значение счётчика: \(counter)"
+    }
+    
+    @IBAction func resetButtonDidTap(_ sender: Any) {
+        counter = 0
+        counterLabelView.text = "Значение счётчика: \(counter)"
+    }
 }
 
